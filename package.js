f595765f9bbd4978a82ca5f046a7e26a452ebe8c
Jessedev1 <jesse@creativework.nl>
2025-06-12 13:18:33 +0200

    Added support for Meteor3.0

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
   name: 'jessedev:cleaner',
   summary: 'Gives you methods to clear your Mongo database and collections for testing purposes',
-  version: '0.4.1',
+  version: '1.0.0',
   git: 'https://github.com/Jessedev1/cleaner.git',
   documentation: 'README.md',
   debugOnly: true,
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
   api.use(['ecmascript', 'mongo', 'underscore']);
   api.addFiles('cleaner.js', ['client', 'server']);
   api.export('resetDatabase', ['client', 'server']);
